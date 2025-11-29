2556d1cf7b2228edc48010f67b15c66f439268e1
Ozgur Akkurt <oezgurmakkurt@gmail.com>
2025-11-29 20:28:50 +0600

    move fastlanes into file

--- src/root.zig
+++ src/root.zig
@@ -1,201 +1,30 @@
 const std = @import("std");
 
-pub const Error = error{Invalid};
+const fastlanes = @import("fastlanes.zig");
+const FastLanes = fastlanes.FastLanes;
 
-const FL_ORDER = [_]usize{ 0, 4, 2, 6, 1, 5, 3, 7 };
-
-fn transpose(comptime T: type, input: [1024]T) [1024]T {
-    var output: [1024]T = undefined;
-    for (0..1024) |i| {
-        output[i] = input[transpose_idx(i)];
-    }
-
-    return output;
-}
-
-fn untranspose(comptime T: type, input: [1024]T) [1024]T {
-    var output: [1024]T = undefined;
-    for (0..1024) |i| {
-        output[transpose_idx(i)] = input[i];
-    }
-
-    return output;
-}
-
-fn transpose_idx(i: usize) usize {
-    const lane = i % 16;
-    const order = (i / 16) % 8;
-    const row = i / 128;
-
-    return (lane * 64) + (FL_ORDER[order] * 8) + row;
-}
-
-fn index(bit_idx: usize, lane: usize) usize {
-    const o = bit_idx / 8;
-    const s = bit_idx % 8;
-    return (FL_ORDER[o] * 16) + (s * 128) + lane;
-}
-
-fn FastLanes(comptime T: type) type {
-    return struct {
-        pub const N_BITS = @sizeOf(T) * 8;
-        pub const N_LANES = 1024 / N_BITS;
-
-        pub fn packed_len(comptime W: comptime_int) comptime_int {
-            return 1024 * W / N_BITS;
-        }
-
-        fn mask(comptime width: comptime_int) T {
-            return comptime (1 << width) - 1;
-        }
-
-        pub fn pack(comptime W: comptime_int, input: [1024]T) [packed_len(W)]T {
-            @setEvalBranchQuota(4096);
-
-            if (W > N_BITS) {
-                @compileError("invalid W");
-            }
-
-            if (W == 0) {
-                return .{};
-            }
-
-            const MASK = mask(W);
-            var out: [packed_len(W)]T = undefined;
-
-            for (0..N_LANES) |lane_idx| {
-                if (W == N_BITS) {
-                    inline for (0..N_BITS) |bit_idx| {
-                        const idx = index(bit_idx, lane_idx);
-                        out[N_LANES * bit_idx + lane_idx] = input[idx];
-                    }
-                } else {
-                    var tmp: T = 0;
-                    inline for (0..N_BITS) |bit_idx| {
-                        const idx = index(bit_idx, lane_idx);
-                        const src = input[idx] & MASK;
-
-                        if (bit_idx == 0) {
-                            tmp = src;
-                        } else {
-                            tmp |= src << (bit_idx * W) % N_BITS;
-                        }
-
-                        const curr_word = bit_idx * W / N_BITS;
-                        const next_word = (bit_idx + 1) * W / N_BITS;
-
-                        if (next_word > curr_word) {
-                            out[N_LANES * curr_word + lane_idx] = tmp;
-                            const remaining_bits = ((bit_idx + 1) * W) % N_BITS;
-                            tmp = src >> W - remaining_bits;
-                        }
-                    }
-                }
-            }
-
-            return out;
-        }
-
-        pub fn unpack(comptime W: comptime_int, input: [packed_len(W)]T) [1024]T {
-            @setEvalBranchQuota(4096);
-
-            if (W > N_BITS) {
-                @compileError("invalid W");
-            }
-
-            if (W == 0) {
-                return std.mem.zeroes([1024]T);
-            }
-
-            var out: [1024]T = undefined;
-
-            for (0..N_LANES) |lane_idx| {
-                if (W == N_BITS) {
-                    inline for (0..N_BITS) |bit_idx| {
-                        const idx = index(bit_idx, lane_idx);
-                        const src = input[N_LANES * bit_idx + lane_idx];
-                        out[idx] = src;
-                    }
-                } else {
-                    var src = input[lane_idx];
-                    var tmp: T = 0;
-
-                    inline for (0..N_BITS) |bit_idx| {
-                        const curr_word = bit_idx * W / N_BITS;
-                        const next_word = (bit_idx + 1) * W / N_BITS;
-
-                        const shift = (bit_idx * W) % N_BITS;
-
-                        if (next_word > curr_word) {
-                            const remaining_bits = ((bit_idx + 1) * W) % N_BITS;
-                            const current_bits = W - remaining_bits;
-                            tmp = (src >> shift) & mask(current_bits);
-
-                            if (next_word < W) {
-                                src = input[N_LANES * next_word + lane_idx];
-                                tmp |= (src & mask(remaining_bits)) << current_bits;
-                            }
-                        } else {
-                            tmp = (src >> shift) & mask(W);
-                        }
-
-                        const idx = index(bit_idx, lane_idx);
-                        out[idx] = tmp;
-                    }
-                }
-            }
-
-            return out;
-        }
-
-        pub fn delta(input: [1024]T, base: [N_LANES]T) [1024]T {
-            var out: [1024]T = undefined;
-
-            for (0..N_LANES) |lane_idx| {
-                var prev = base[lane_idx];
-
-                inline for (0..N_BITS) |bit_idx| {
-                    const idx = index(bit_idx, lane_idx);
-                    const next = input[idx];
-                    out[idx] = next -% prev;
-                    prev = next;
-                }
-            }
-
-            return out;
-        }
-
-        pub fn undelta(input: [1024]T, base: [N_LANES]T) [1024]T {
-            var out: [1024]T = undefined;
-
-            for (0..N_LANES) |lane_idx| {
-                var prev = base[lane_idx];
-
-                inline for (0..N_BITS) |bit_idx| {
-                    const idx = index(bit_idx, lane_idx);
-                    const next = input[idx] +% prev;
-                    out[idx] = next;
-                    prev = next;
-                }
-            }
-
-            return out;
-        }
-    };
-}
+// pub const Error = error{Invalid};
 
 // fn Zint(comptime T: type) type {
 //     const FL = FastLanes(T);
 
+//     const Header = packed struct(u8) {
+//         is_delta: u1,
+//         _padding: u1,
+//         num_bits: u6,
+//     };
+
 //     return struct {
+//         fn max(data: [1024]T) T {
+//             var m = @abs(data[0]);
+//             for (0..1024) |idx| {
+//                 m = @max(data[idx], @abs(m));
+//             }
+//             return m;
+//         }
+
 //         pub fn pack(input: [1024]T, noalias out: []u8) usize {
 //             std.debug.assert(out.len >= 1 + @sizeOf(T) * 1024);
-
-//             var max = input[0];
-//             for (0..1024) |idx| {
-//                 max = @max(input[idx], max);
-//             }
-
 //             const num_bits: u8 = std.math.log2_int_ceil(T, max);
 
 //             out[0] = num_bits;
@@ -241,52 +70,6 @@ fn FastLanes(comptime T: type) type {
 //     };
 // }
 
-test "pack to" {
-    var data: [1024]u64 = undefined;
-    for (0..1024) |i| {
-        data[i] = i;
-    }
-
-    const FL = FastLanes(u64);
-
-    const packed_d = FL.pack(10, data);
-
-    const unpacked_d = FL.unpack(10, packed_d);
-
-    for (0..1024) |i| {
-        std.debug.assert(unpacked_d[i] == data[i]);
-    }
-}
-
-test "delta pack to" {
-    const base = 213123213;
-
-    var data: [1024]u64 = undefined;
-    for (0..1024) |i| {
-        data[i] = @intCast(i + base);
-    }
-
-    const FL = FastLanes(u64);
-
-    const transposed_d = transpose(u64, data);
-
-    const bases = transposed_d[0..FL.N_LANES].*;
-
-    const delta_d = FL.delta(transposed_d, bases);
-
-    const packed_d = FL.pack(1, delta_d);
-
-    const unpacked_d = FL.unpack(1, packed_d);
-
-    const undelta_d = FL.undelta(unpacked_d, bases);
-
-    const untransposed_d = untranspose(u64, undelta_d);
-
-    for (0..1024) |i| {
-        std.debug.assert(untransposed_d[i] == data[i]);
-    }
-}
-
 // test "zint pack" {
 //     var data: [1024]u64 = undefined;
 //     for (0..1024) |i| {
@@ -306,3 +89,8 @@ test "delta pack to" {
 //         std.debug.assert(unpacked_d[i] == data[i]);
 //     }
 // }
+//
+
+test {
+    _ = fastlanes;
+}
